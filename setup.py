8b900a53b34a0d1e3a4a7487de06439287857758
Jolinda-hub <60364539+Jolinda-hub@users.noreply.github.com>
2020-02-17 11:22:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='EDSA example python package',
+    description='EDSA - TEAM9 Predict Package',
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas'],
     url='https://github.com/MJSteenberg/mypackage',
