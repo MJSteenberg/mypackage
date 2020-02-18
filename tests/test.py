39d681a49ca63a8ecbb02f9bdb793c33f8c5eb0d
MJSteenberg <60359158+MJSteenberg@users.noreply.github.com>
2020-02-18 12:31:47 +0200

    Updated tests

---
+++ tests/test.py
@@ -0,0 +1,23 @@
+import mypackage.myModule as mm
+
+ # Function 1 Test
+ print(mm.dictionary_of_metrics([1,2,3,4,5]))
+ 
+ # Function 2 Test
+ print(mm.five_num_summary([1,2,3,4,5))
+ 
+ # Function 3 Test
+ print(mm.date_parser(dates[:3]))
+ 
+ # Function 4 Test
+ print(mm.extract_municipality_hashtags(twitter_df.copy()))
+ 
+ # Function 5 Test
+ print(mm.number_of_tweets_per_day(twitter_df.copy()))
+ 
+ # Function 6 Test
+ print(mm.word_splitter(twitter_df.copy()))
+ 
+ # Function 7 Test
+ print(mm.stop_words_remover(twitter_df.copy()))
+ 
